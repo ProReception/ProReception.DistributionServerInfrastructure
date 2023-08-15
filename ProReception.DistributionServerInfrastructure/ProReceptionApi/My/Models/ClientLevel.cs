1504ad7e25e3e8791ce4f9819784574833761b1a
Søren Pedersen <sh.pedersen@gmail.com>
2023-08-15 12:14:22 +0200

    Add MyExtensions to enable getting the client levels for the authenticated user

---
+++ ProReception.DistributionServerInfrastructure/ProReceptionApi/My/Models/ClientLevel.cs
@@ -0,0 +1,12 @@
+﻿namespace ProReception.DistributionServerInfrastructure.ProReceptionApi.My.Models;
+
+public class ClientLevel
+{
+    public int ClientLevelId { get; set; }
+
+    public short ClientLevelHierarchyLevel { get; set; }
+
+    public required string Name { get; set; }
+
+    public required string Hierarchy { get; set; }
+}
---
+++ ProReception.DistributionServerInfrastructure/ProReceptionApi/My/MyExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace ProReception.DistributionServerInfrastructure.ProReceptionApi.My;
+
+using JetBrains.Annotations;
+using Models;
+
+[PublicAPI]
+public static class MyExtensions
+{
+    public static async Task<List<ClientLevel>> GetMyClientLevels(this IProReceptionApiClient proReceptionApiClient)
+        => await proReceptionApiClient.Get<List<ClientLevel>>("my/client-levels");
+}
