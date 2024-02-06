1b5a1fdbae5201859ef820cb337b41485cffa895
Søren Pedersen <sh.pedersen@gmail.com>
2024-02-06 09:06:05 +0100

    Add api extension for getting my client sites

---
+++ ProReception.DistributionServerInfrastructure/ProReceptionApi/My/Models/ClientSite.cs
@@ -0,0 +1,8 @@
+﻿namespace ProReception.DistributionServerInfrastructure.ProReceptionApi.My.Models;
+
+public class ClientSite
+{
+    public int ClientSiteId { get; set; }
+
+    public required string Name { get; set; }
+}
--- ProReception.DistributionServerInfrastructure/ProReceptionApi/My/MyExtensions.cs
+++ ProReception.DistributionServerInfrastructure/ProReceptionApi/My/MyExtensions.cs
@@ -8,4 +8,7 @@ public static class MyExtensions
 {
     public static async Task<List<ClientLevel>> GetMyClientLevels(this IProReceptionApiClient proReceptionApiClient)
         => await proReceptionApiClient.Get<List<ClientLevel>>("my/client-levels");
+
+    public static async Task<List<ClientSite>> GetMyClientSites(this IProReceptionApiClient proReceptionApiClient)
+        => await proReceptionApiClient.Get<List<ClientSite>>("my/client-sites");
 }
