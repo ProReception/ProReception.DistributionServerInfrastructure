a9631857e57cda9af8692ab18930150a444b669e
Søren Pedersen <sh.pedersen@gmail.com>
2024-02-06 14:15:17 +0100

    Add SaveNoxDoors api endpoint extension method

---
+++ ProReception.DistributionServerInfrastructure/ProReceptionApi/NoxConnector/Models/SaveNoxDoorsRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace ProReception.DistributionServerInfrastructure.ProReceptionApi.NoxConnector.Models;
+
+using JetBrains.Annotations;
+
+[PublicAPI]
+public class SaveNoxDoorsRequest
+{
+    public int ClientSiteId { get; set; }
+
+    public List<NoxDoor> NoxDoors { get; set; } = new();
+
+    public class NoxDoor
+    {
+        public required string Name { get; set; }
+
+        public int NoxAreaId { get; set; }
+    }
+}
