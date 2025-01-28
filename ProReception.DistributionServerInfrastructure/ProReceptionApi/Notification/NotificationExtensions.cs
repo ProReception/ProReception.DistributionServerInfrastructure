3b2c53dd12a5fafea682dd262ed8bd17c72a44e4
Søren Pedersen <sh.pedersen@gmail.com>
2025-01-28 13:05:01 +0100

    https://trello.com/c/ELHZN5CE Add SendPrinterProblemNotification API call

---
+++ ProReception.DistributionServerInfrastructure/ProReceptionApi/Notification/NotificationExtensions.cs
@@ -0,0 +1,10 @@
+﻿using JetBrains.Annotations;
+
+namespace ProReception.DistributionServerInfrastructure.ProReceptionApi.Notification;
+
+[PublicAPI]
+public static class NotificationExtensions
+{
+    public static async Task SendPrinterProblemNotification(this IProReceptionApiClient proReceptionApiClient)
+        => await proReceptionApiClient.Post("notification/printer-problem", new object());
+}
